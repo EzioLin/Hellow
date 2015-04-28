eac1ed43ee2c3aa0138138639d3b4f0693c3063e
EzioLin <Ezio.Lin@Gaiaworks.cn>
2015-04-28 14:03:03 +0800

    first
    第一次

---
+++ HellowWorld/HellowWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellowWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hellow world");
+            Console.WriteLine("hellow world again");
+            Console.WriteLine("hellow world ggg");
+        }
+    }
+}
